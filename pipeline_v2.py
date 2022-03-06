bb8e9fc6788b9ad1ee42e4fc3c391aa1bfc9a3fc
unknown <joselb1993@gmail.com>
2022-03-06 20:37:45 +0100

    run ann

--- pipeline_v2.py
+++ pipeline_v2.py
@@ -80,7 +80,7 @@ https://scikit-learn.org/stable/modules/model_evaluation.html#scoring-parameter
 '''
 print("[INFO] performing random search...")
 searcher = RandomizedSearchCV(estimator=model, n_jobs=-1, cv=3,
-	param_distributions=grid, scoring="neg_mean_squared_error", verbose = 10)
+	param_distributions=grid, scoring="neg_mean_squared_error", verbose = 20)
 searchResults = searcher.fit(X_train, y_train)
 
 # summarize grid search information
