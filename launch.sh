0d6dae914f101a98e2dfb2c2f854f7588d8fbee2
unknown <joselb1993@gmail.com>
2022-03-06 22:07:46 +0100

    run ann

--- launch.sh
+++ launch.sh
@@ -3,7 +3,7 @@
 #SBATCH -p shared
 #SBATCH -c 16
 #SBATCH --qos shared_short
-#SBATCH --mem=10
+#SBATCH --mem 32GB
 #SBATCH -t 01:00:00
 
 module load cesga/2018 gcc/6.4.0 pandas/1.0.0-python-3.8.1 tensorflow/2.2.1-python-3.8.1
