fda8c579c9ff3489e587667203b9731d228f91a7
unknown <joselb1993@gmail.com>
2022-03-06 19:58:17 +0100

    run ann

--- launch.sh
+++ launch.sh
@@ -5,5 +5,6 @@
 #SBATCH --qos shared_short
 
 compute --mem 10
-module load cesga/2018 gcc/6.4.0 tensorflow/2.2.1-python-3.8.1 seaborn/0.10.0-python-3.8.1
-python pipeline.py
+module load cesga/2018 gcc/6.4.0 pandas/1.0.0-python-3.8.1 tensorflow/2.2.1-python-3.8.1
+
+python pipeline_v2.py
