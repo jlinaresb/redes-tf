695280bbeb147288d94c450a74942d4b75ccaf28
unknown <joselb1993@gmail.com>
2022-03-08 00:07:50 +0100

    slurm

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-files=`ls /mnt/netapp2/Store_uni/home/ulc/co/jlb/redes-tf/data/*.csv`
-for i in $files
+for i in `ls /mnt/netapp2/Store_uni/home/ulc/co/jlb/redes-tf/data/*.csv`
 do
-	echo -f $i
+	echo now is $i
+	cat $i
 done
