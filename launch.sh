5cae78d86171d8fb8811e2303189ddf16803a40d
unknown <joselb1993@gmail.com>
2022-03-08 00:25:09 +0100

    slurm

--- launch.sh
+++ launch.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-#SBATCH -p shared
+#SBATCH -p cola-corta
 #SBATCH -c 16
-#SBATCH --qos shared_short
-#SBATCH --mem 32GB
+#SBATCH --qos default
+#SBATCH --mem 8GB
 #SBATCH -t 01:00:00
 
 module load cesga/2018 gcc/6.4.0 pandas/1.0.0-python-3.8.1 tensorflow/2.2.1-python-3.8.1
