40f9110c731c738da15f7b8335fcd54fc10d5ec2
unknown <joselb1993@gmail.com>
2022-03-08 00:59:25 +0100

    slurm

--- run.sh
+++ run.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
-dir="/mnt/netapp2/Store_uni/home/ulc/co/jlb/redes-tf/data/"
-count=0
-for i in $dir/*
-do
-        count=$((count+1))
-done
 
-for i in `seq count`
+for i in `seq 4`
 do
         sbatch launch.sh $i
 done
