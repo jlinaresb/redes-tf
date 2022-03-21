df5d5ef6e3045241d43c0a8e853fdafcacc89b5e
jlinaresb <joselb1993@gmail.com>
2022-03-21 16:14:00 +0100

    launch first experiment

--- launch.sh
+++ launch.sh
@@ -3,7 +3,7 @@
 #SBATCH -p shared
 #SBATCH -c 16
 #SBATCH --qos shared_short
-#SBATCH --mem 8GB
+#SBATCH --mem 32GB
 #SBATCH -t 01:00:00
 
 module load cesga/2018 gcc/6.4.0 pandas/1.0.0-python-3.8.1 scipy/1.4.1-python-3.8.1 tensorflow/2.2.1-python-3.8.1
